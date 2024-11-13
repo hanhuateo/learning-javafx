2e7e96abb10c1820a46549aa62b9a7422cf4ddff
hanhuateo <hanhuateo@gmail.com>
2024-11-13 16:14:45 +0800

    adding in label and scene, on top of stage

--- learn-javafx/src/main/java/org/thh/App.java
+++ learn-javafx/src/main/java/org/thh/App.java
@@ -2,6 +2,8 @@ package org.thh;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 
@@ -13,6 +15,11 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("JavaFX App");
+        
+        Label label = new Label("Hello World, JavaFX");
+        Scene scene = new Scene(label, 400, 200);
+        stage.setScene(scene);
+
         stage.show();
     }
 
