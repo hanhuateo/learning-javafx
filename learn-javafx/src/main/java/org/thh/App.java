325045c575739a4edaed284b7b96036c75d19c6b
hanhuateo <hanhuateo@gmail.com>
2024-11-13 16:12:08 +0800

    understand stage

--- learn-javafx/src/main/java/org/thh/App.java
+++ learn-javafx/src/main/java/org/thh/App.java
@@ -1,11 +1,7 @@
 package org.thh;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
 
 import java.io.IOException;
 
@@ -14,27 +10,10 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
     public void start(Stage stage) throws IOException {
-        // scene = new Scene(loadFXML("primary"), 640, 480);
-        // stage.setScene(scene);
-        // stage.show();
-        stage.setTitle("My First JavaFX App");
-        Label label = new Label("Hello World, JavaFX!");
-        Scene scene = new Scene(label, 400, 200);
-        stage.setScene(scene);
+        stage.setTitle("JavaFX App");
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
