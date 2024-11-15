d9fd67aa22726b0ef9cf9216e5633b8a2dfa4d5d
hanhuateo <hanhuateo@gmail.com>
2024-11-15 10:21:52 +0800

    added toPercentage function to CalculatorController.java

--- demo/src/main/java/com/example/CalculatorController.java
+++ demo/src/main/java/com/example/CalculatorController.java
@@ -13,9 +13,47 @@ public class CalculatorController {
 
     private String actualText;
 
+    private String[] operator = new String[1];
+
     @FXML
     public void handleClick(Event event) {
-
+        actualText = display.getText();
+        Button btn = (Button) event.getSource();
+        switch (btn.getId()) {
+            case "zero":
+                display.appendText("0");
+                break;
+            case "one":
+                display.appendText("1");
+                break;
+            case "two":
+                display.appendText("2");
+                break;
+            case "three":
+                display.appendText("3");
+                break;
+            case "four":
+                display.appendText("4");
+                break;
+            case "five":
+                display.appendText("5");
+                break;
+            case "six":
+                display.appendText("6");
+                break;
+            case "seven":
+                display.appendText("7");
+                break;
+            case "eight":
+                display.appendText("8");
+                break;
+            case "nine":
+                display.appendText("9");
+                break;
+            case "decimal":
+                display.appendText(".");
+                break;
+        }
     }
 
     @FXML
@@ -28,6 +66,22 @@ public class CalculatorController {
                 System.out.println(number);
             } 
             else {
+                return;
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void toPercentage() {
+        double number = Double.parseDouble(display.getText());
+        try {
+            if (number != 0) {
+                number = number / 100;
+                display.setText(Double.toString(number));
+                System.out.println(number);
+            } else {
                 return;
             }
         } catch (NumberFormatException e) {
